38c3326ff0cd844892f7422a381dbb2a2ebfdc98
Masedi Ncamani <masedi.ncamani@gmail.com>
2024-01-12 15:56:58 +0200

    Update GitLink.js

--- src/GitLink.js
+++ src/GitLink.js
@@ -3,12 +3,8 @@ import React from "react";
 export default function GitLink() {
   return (
     <div>
-      <a href="https://github.com/huntesq/final-react-app" target="_blank">
-        Open-source
-      </a>{" "}
-      code by{" "}
-      <a href="https://tiny-cheesecake-17c592.netlify.app/" target="_blank">
-        Masedi Ncamani
+      <a href="https://github.com/huntesq/final-react-app" target="_blank" rel="noreferrer">
+        Github Repository
       </a>
     </div>
   );
